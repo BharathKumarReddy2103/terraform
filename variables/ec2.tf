54fb09906ade1b47dc6d1b6dc2ba32a5d6e5a1ce
BharathKumarReddy <nbkumar2103@gmail.com>
2025-06-10 05:50:50 +0530

    updated .tf file

--- variables/ec2.tf
+++ variables/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "roboshop" {
   ami           = var.ami_id
-  instance_type = var.instance_type
+  instance_type = var.instance_type # left and right side names no need to be same
   vpc_security_group_ids = [ aws_security_group.allow_all.id ]
   
   tags = var.ec2_tags
