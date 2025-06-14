8d536acf5e695fe3843881986e2550bdfac56a23
BharathKumarReddy <nbkumar2103@gmail.com>
2025-06-14 16:28:52 +0530

    updated .tf file

--- data-sources/data.tf
+++ data-sources/data.tf
@@ -2,22 +2,22 @@ data "aws_ami" "devops" {
   owners           = ["334864977755"]
   most_recent      = true
 
-#   filter {
-#     name   = "name"
-#     values = ["RHEL-9-DevOps-Practice"]
-#   }
+  filter {
+    name   = "name"
+    values = ["RHEL-9-DevOps-Practice"]
+  }
 
-#   filter {
-#     name   = "root-device-type"
-#     values = ["ebs"]
-#   }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
 
-#   filter {
-#     name   = "virtualization-type"
-#     values = ["hvm"]
-#   }
-# }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
 
-# output "ami_id" {
-#   value       = data.aws_ami.devops.id
-# }
+output "ami_id" {
+  value       = data.aws_ami.devops.id
+}
