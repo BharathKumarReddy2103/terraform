192919eaff0382b22217326ebbd833cec26dd077
BharathKumarReddy <nbkumar2103@gmail.com>
2025-06-22 16:25:59 +0530

    updated variable.tf

--- variables/variables.tf
+++ variables/variables.tf
@@ -8,38 +8,38 @@ variable "instance_type" {
   default = "t3.micro"
 }
 
-# variable "ec2_tags" {
-#   type = map(string)
-#   default = {
-#       Name = "HelloWorld"
-#       Purpose = "variables-demo"
-#   }
-# }
+variable "ec2_tags" {
+  type = map(string)
+  default = {
+      Name = "HelloWorld"
+      Purpose = "variables-demo"
+  }
+}
 
-# variable "sg_name" {
-#   default = "vars-file-allow-all-roboshop"
-# }
+variable "sg_name" {
+  default = "allow-all-roboshop"
+}
 
-# variable "sg_description" {
-#   default = "allowing all ports from internet"
-# }
+variable "sg_description" {
+  default = "allowing all ports from internet"
+}
 
-# variable "from_port" {
-#   default = 0
-# }
+variable "from_port" {
+  default = 0
+}
 
-# variable "to_port" {
-#   type = number
-#   default = 0
-# }
+variable "to_port" {
+  type = number
+  default = 0
+}
 
-# variable "cidr_blocks" {
-#   type = list(string)
-#   default = ["0.0.0.0/0"]
-# }
+variable "cidr_blocks" {
+  type = list(string)
+  default = ["0.0.0.0/0"]
+}
 
-# variable "sg_tags" {
-#   default = {
-#       Name = "allow-all"
-#   }
-# }
+variable "sg_tags" {
+  default = {
+      Name = "allow-all-roboshop"
+  }
+}
