7ab1c6629fbc493a762d61f7b1acbb5f35a88c0b
BharathKumarReddy <nbkumar2103@gmail.com>
2025-06-22 18:07:13 +0530

    updated variable.tf

--- for-loop/variables.tf
+++ for-loop/variables.tf
@@ -49,13 +49,13 @@ variable "environment" {
 }
 
 variable "instances" {
-    /* default = {
+    default = {
         mongodb = "t3.micro" # each keyword is assigned for every iteration. you will get each.key and each.value
         redis = "t3.micro"
         mysql = "t3.small"
         rabbitmq = "t3.micro"
-    } */
-    default = ["mongodb", "redis", "mysql", "rabbitmq"]
+    }
+    # default = ["mongodb", "redis", "mysql", "rabbitmq"]
 }
 
 variable "zone_id" {
