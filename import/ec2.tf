f96150166e02cd091429678c3cc83bd451bfa588
BharathKumarReddy <nbkumar2103@gmail.com>
2025-07-11 19:45:50 +0530

    updated .tf file

--- import/ec2.tf
+++ import/ec2.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "linux" {
   ami = "ami-09c813fb71547fc4f"
   instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg-035143f391bb55883"]
+  vpc_security_group_ids = ["sg-05847b88288dddafe"]
 
   tags = {
-    Name = "linux-change"
+    Name = "linux"
   }
 }
