a07e52396b499850e067c11b3884bc29b964159c
BharathKumarReddy <nbkumar2103@gmail.com>
2025-07-22 06:07:54 +0530

    updated file

--- functions/r53.tf
+++ functions/r53.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "www" {
   count = length(var.instances)
   zone_id = var.zone_id
-  name    = "${var.instances[count.index]}.${var.domain_name}" #mongodb.daws84s.site
+  name    = "${var.instances[count.index]}.${var.domain_name}" #mongodb.nbkumar2103.site
   type    = "A"
   ttl     = 1
   records = [aws_instance.roboshop[count.index].private_ip]
