1c7eb4512b7b3a8595c8a338b3f04bb86f70ca5d
BharathKumarReddy <nbkumar2103@gmail.com>
2025-07-31 18:42:30 +0530

    updated file

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "roboshop" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.micro"
+   subnet_id   = "subnet-027e9b95abbffe496"
   vpc_security_group_ids = [ aws_security_group.allow_all.id ]
   
   tags = {
