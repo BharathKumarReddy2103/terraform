55264cf29934e49b690741d81e72834d2f1b54f0
BharathKumarReddy <nbkumar2103@gmail.com>
2025-07-31 22:47:11 +0530

    updated file

--- data-sources/ec2.tf
+++ data-sources/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "roboshop" {
-  ami           = data.aws_ami.bharathdevops.id
+  ami           = data.aws_ami.joindevops.id
   instance_type = "t3.micro"
   vpc_security_group_ids = [ aws_security_group.allow_all.id ]
   
