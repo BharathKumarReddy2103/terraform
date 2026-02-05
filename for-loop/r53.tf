e0f482499f1d65e62f9a60e53b2bc9cb217f6309
BharathKumarReddy <nbkumar2103@gmail.com>
2026-02-05 07:40:55 +0530

    updated file

--- for-loop/r53.tf
+++ for-loop/r53.tf
@@ -1,8 +1,8 @@
-resource "aws_route53_record" "www" {
-  for_each = aws_instance.roboshop
-  zone_id = var.zone_id
-  name    = "${each.key}.${var.domain_name}" #mongodb.bharath2103.site
-  type    = "A"
-  ttl     = 1
-  records = [each.value.private_ip]
-}
+# resource "aws_route53_record" "www" {
+#   for_each = aws_instance.roboshop
+#   zone_id = var.zone_id
+#   name    = "${each.key}.${var.domain_name}" #mongodb.bharath2103.site
+#   type    = "A"
+#   ttl     = 1
+#   records = [each.value.private_ip]
+# }
