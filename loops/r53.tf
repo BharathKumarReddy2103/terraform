02e0aa967b832cfbf7791ffa4ac24a20a16ce90d
BharathKumarReddy <nbkumar2103@gmail.com>
2026-02-06 06:52:48 +0530

    updated file

--- loops/r53.tf
+++ loops/r53.tf
@@ -1,8 +1,8 @@
-resource "aws_route53_record" "www" {
-  count = 5
-  zone_id = var.zone_id
-  name    = "${var.instances[count.index]}.${var.domain_name}" #mongodb.bharath2103.site
-  type    = "A"
-  ttl     = 1
-  records = [aws_instance.roboshop[count.index].private_ip]
-}
+# resource "aws_route53_record" "www" {
+#   count = 5
+#   zone_id = var.zone_id
+#   name    = "${var.instances[count.index]}.${var.domain_name}" #mongodb.bharath2103.site
+#   type    = "A"
+#   ttl     = 1
+#   records = [aws_instance.roboshop[count.index].private_ip]
+# }
