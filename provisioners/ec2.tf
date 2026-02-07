ad639d18af1b58cfedd873fc6c4634a34f131c52
BharathKumarReddy <nbkumar2103@gmail.com>
2026-02-07 17:28:29 +0530

    updated file

--- provisioners/ec2.tf
+++ provisioners/ec2.tf
@@ -9,35 +9,35 @@ resource "aws_instance" "roboshop" {
 
   provisioner "local-exec" {
     command = "${self.private_ip} > inventory"
-    on_failure = continue #ignoring errors
+    # on_failure = continue #ignoring errors
   }
 
-  provisioner "local-exec" {
-    command = "echo 'instance is destroyed'"
-    when = destroy
-  }
+#   provisioner "local-exec" {
+#     command = "echo 'instance is destroyed'"
+#     when = destroy
+#   }
 
-  connection {
-    type     = "ssh"
-    user     = "ec2-user"
-    password = "DevOps321"
-    host     = self.public_ip
-  }
+#   connection {
+#     type     = "ssh"
+#     user     = "ec2-user"
+#     password = "DevOps321"
+#     host     = self.public_ip
+#   }
 
-  provisioner "remote-exec" {
-     inline = [
-       "sudo dnf install nginx -y",
-       "sudo systemctl start nginx",
-    ]
-}
+#   provisioner "remote-exec" {
+#      inline = [
+#        "sudo dnf install nginx -y",
+#        "sudo systemctl start nginx",
+#     ]
+# }
 
-  provisioner "remote-exec" {
-    when = destroy
-    inline = [
-      "sudo systemctl stop nginx"
-    ]
-  }
-}
+#   provisioner "remote-exec" {
+#     when = destroy
+#     inline = [
+#       "sudo systemctl stop nginx"
+#     ]
+#   }
+# }
 
 resource "aws_security_group" "allow_all" {
     name        = var.sg_name
